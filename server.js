d361b4b17fa90bc2575e46912a348d65529f9fdb
mohamednoufal30 <mohamednoufal789@gmail.com>
2025-05-06 17:02:40 +0530

    Update: latest code changes

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/protected', authenticateToken, (req, res) => {
 
 // Start the server
 // const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 
 
